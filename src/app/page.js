a5219c6a2c03abeb58ee640bf0a25bca5e05f84e
Solongooyaa <u.solongo2018@gmail.com>
2024-11-25 17:50:53 +0800

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+
+export default function Home() {
+  let counter = 0;
+
+  const handleClick = () => {};
+
+  return (
+    <div>
+      <button onClick={handleClick}>click</button>
+    </div>
+  );
+}
